1753c6e7fea38bfbcac956f4d2ab2450d23a9db0
Howard Butler <howard@hobu.co>
2017-01-05 09:53:55 -0600

    update build script thanks to michacochran's note about UTF-8 #1

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ tables=$(ls *_Tables_*.sql)
 # CHR function in PostgreSQL is called Char in sqlite
 gsed -i 's/CHR/Char/g' $data
 
+iconv -f ISO-8859-1 -t UTF-8 $data > $data.converted
 $sql $dbname < $tables
-$sql $dbname < $data
+$sql $dbname < $data.converted
 
 
